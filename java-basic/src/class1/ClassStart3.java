b72495b98ac0ec5d5c4c1e3cfc03fba716114f84
leehyeonwoo <justice3350@naver.com>
2025-10-23 18:00:14 +0900

    Java basic 1023

--- java-basic/src/class1/ClassStart3.java
+++ java-basic/src/class1/ClassStart3.java
@@ -4,7 +4,7 @@ public class ClassStart3 {
 
     public static void main(String[] args) {
         Student student1;
-        student1 = new Student(); //x001
+        student1 = new Student(); //x001 studen1 참조변수.. 힙영역 메모리에 저장되는 객체를 가르키는 변수
         student1.name = "학생1";
         student1.age = 15;
         student1.grade = 90;
@@ -13,6 +13,9 @@ public class ClassStart3 {
         student2.name = "학생2";
         student2.age = 16;
         student2.grade = 80;
+
+        System.out.println(student1); //2f4d3709 -> 참조값
+        System.out.println(student2); //4e50df2e -> 참조값.. 두개는 서로 다른 객체이다!
 
         System.out.println("이름:" + student1.name + " 나이:" + student1.age + " 성적:" + student1.grade);
         System.out.println("이름:" + student2.name + " 나이:" + student2.age + " 성적:" + student2.grade);
