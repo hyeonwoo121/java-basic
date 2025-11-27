d51a215c4b75c5e290cb38d41a1f0ef2ff6f1e8a
leehyeonwoo <justice3350@naver.com>
2025-11-27 18:13:34 +0900

    상속super 1127

--- java-basic/src/extends1/super2/ClassB.java
+++ java-basic/src/extends1/super2/ClassB.java
@@ -8,7 +8,7 @@ public class ClassB extends ClassA {
     }
 
     public ClassB(int a, int b) {
-        super(); //부모 클래스의 생성자가 기본생성자(파라미터가없는)인 경우에는 super생략 가능.
+                super(); //부모 클래스의 생성자가 기본생성자(파라미터가없는)인 경우에는 super생략 가능.
         System.out.println("ClassB 생성자 a=" + a + " b=" + b);
     }
 }
--- java-basic/src/memory/JavaMemoryMain2.java
+++ java-basic/src/memory/JavaMemoryMain2.java
@@ -15,6 +15,8 @@ public class JavaMemoryMain2 {
         System.out.println("method1 end");
     }
 
+    //data1이 갖고 있던 객체의 주소값을 그대로 복사해서 data2에 넣기 때문에
+    //결국 data2도 value=10을 보유한 Data 객체를 가리키게 되는 것.
     static void method2(Data data2) {
         System.out.println("method2 start");
         System.out.println("data.value=" + data2.getValue());
